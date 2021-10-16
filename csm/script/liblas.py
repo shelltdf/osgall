c2db671445e95e65e15dd6b06c3037dad02675e2
will <shell_tdf@126.com>
2021-10-16 23:28:04 +0800

    调整linux脚本

--- csm/script/liblas.py
+++ csm/script/liblas.py
@@ -28,28 +28,52 @@ def SBI( str_name , b_only_download ,dict_config, getLibrary ):
     install_dir = os.getcwd().replace('\\','/') +"/install/"+ my_build_and_install_dir(dict_config)
 
     STR_CFG = ''
-    STR_CFG += " -DWITH_TESTS=0"
-    STR_CFG += " -DWITH_UTILITIES=0"
-    STR_CFG += " -DBUILD_OSGEO4W=0"
-    if(dict_config['static']):
-        STR_CFG += " -DBUILD_STATIC_LIBRARY=1"
-        STR_CFG += " -DBoost_USE_STATIC_LIBS=1"
-        
-        if(dict_config['release']):
-            STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.lib'"
-        else:
-            STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_d.lib'"
+    
+    if(dict_config['arch'][:2]=="vs"):
+        STR_CFG += " -DWITH_TESTS=0"
+        STR_CFG += " -DWITH_UTILITIES=0"
+        STR_CFG += " -DBUILD_OSGEO4W=0"
+        if(dict_config['static']):
+            STR_CFG += " -DBUILD_STATIC_LIBRARY=1"
+            STR_CFG += " -DBoost_USE_STATIC_LIBS=1"
             
-    else:
-        STR_CFG += " -DBUILD_STATIC_LIBRARY=0"
-        STR_CFG += " -DBoost_USE_STATIC_LIBS=0"
-        
-        if(dict_config['release']):
-            STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_i.lib'"
+            if(dict_config['release']):
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.lib'"
+            else:
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_d.lib'"
+                
         else:
-            STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_d_i.lib'"
-        
-        
+            STR_CFG += " -DBUILD_STATIC_LIBRARY=0"
+            STR_CFG += " -DBoost_USE_STATIC_LIBS=0"
+            
+            if(dict_config['release']):
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_i.lib'"
+            else:
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff_d_i.lib'"
+            
+    if(dict_config['arch']=="unix"):
+        STR_CFG += " -DWITH_TESTS=0"
+        STR_CFG += " -DWITH_UTILITIES=0"
+        STR_CFG += " -DBUILD_OSGEO4W=0"
+        if(dict_config['static']):
+            STR_CFG += " -DBUILD_STATIC_LIBRARY=1"
+            STR_CFG += " -DBoost_USE_STATIC_LIBS=1"
+            
+            if(dict_config['release']):
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.so'"
+            else:
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.so'"
+                
+        else:
+            STR_CFG += " -DBUILD_STATIC_LIBRARY=0"
+            STR_CFG += " -DBoost_USE_STATIC_LIBS=0"
+            
+            if(dict_config['release']):
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.so'"
+            else:
+                STR_CFG += " -DGEOTIFF_LIBRARY='" + install_dir + "/lib/geotiff.so'"
+
+    
     source_dir = os.getcwd() + '/../prebuild/libLAS-1.8.1'
     
     configure(str_name,dict_config,STR_CFG,"",source_dir)
