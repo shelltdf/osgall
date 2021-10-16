73ed4c1089d26bae730008e51feb4b2d009f86fd
will <shell_tdf@126.com>
2021-10-16 19:23:47 +0800

    添加linux编译脚本

---
+++ csm/build_linux_gcc.bat
@@ -0,0 +1,6 @@
+
+python cmake-git.py install all -arch unix
+python cmake-git.py install all -arch unix -debug
+
+python cmake-git.py install all -arch unix -static
+python cmake-git.py install all -arch unix -debug -static
