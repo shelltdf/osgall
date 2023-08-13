1529c75dd3934a863cb3ff49908b107279f8e3f6
shell_tdf <shell_tdf@126.com>
2023-08-13 22:27:16 +0800

    降级到 openexr 2 版本

--- csm/script/openexr.py
+++ csm/script/openexr.py
@@ -5,9 +5,13 @@ def getDependency( str_name ,getDependency):
     list_name = []
     
     list_name = addDependency("zlib" , list_name,getDependency)
-    # list_name = addDependency("ilmbase" , list_name,getDependency)
-    list_name = addDependency("imath" , list_name,getDependency)
-    list_name = addDependency("libdeflate" , list_name,getDependency)
+    
+    # 2
+    list_name = addDependency("ilmbase" , list_name,getDependency)
+    
+    # 3
+    # list_name = addDependency("imath" , list_name,getDependency)
+    # list_name = addDependency("libdeflate" , list_name,getDependency)
     
     return list_name + [str_name]
     
@@ -84,8 +88,8 @@ def SBI( str_name , b_only_download ,dict_config, getLibrary ):
         STR_CFG += ' -DPACK_BINARY_NSIS=OFF'
 
     
-    # source_dir = os.getcwd() + '/../prebuild/openexr-2.2.0'
-    source_dir = os.getcwd() + '/../prebuild/openexr-main'
+    source_dir = os.getcwd() + '/../prebuild/openexr-2.2.0'
+    # source_dir = os.getcwd() + '/../prebuild/openexr-main'
     
     configure(str_name,dict_config,STR_CFG,"",source_dir)
     build(str_name,dict_config)
