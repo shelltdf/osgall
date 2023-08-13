fe98cba96c031923a0c0b44fc493c998b84303ca
shell_tdf <shell_tdf@126.com>
2023-08-13 23:47:09 +0800

    add zstd cmake script

---
+++ csm/script/zstd.py
@@ -0,0 +1,34 @@
+
+from ._common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    # if(b_only_download):
+        # download_source(str_name,"https://github.com/madler/zlib.git")
+        # return
+        
+    STR_CFG = ''
+    STR_CFG += ' -DBUILD_TESTS=0'
+    if(dict_config['static']):
+        STR_CFG += " -DZSTD_BUILD_STATIC=1"
+        STR_CFG += " -DZSTD_BUILD_SHARED=0"
+        STR_CFG += " -DZSTD_BUILD_PROGRAMS=1"
+    else:
+        STR_CFG += " -DZSTD_BUILD_STATIC=0"
+        STR_CFG += " -DZSTD_BUILD_SHARED=1"
+        STR_CFG += " -DZSTD_BUILD_PROGRAMS=0"
+        
+    source_dir = os.getcwd() + '/../prebuild/zstd-dev/build/cmake'
+    
+    configure(str_name,dict_config,STR_CFG,"",source_dir)
+    build(str_name,dict_config)
+    install(str_name,dict_config)
+    
+    
