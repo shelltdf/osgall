2681b0d83aa1ecea8ce30d0b846ef861f6956292
shell_tdf <shell_tdf@126.com>
2023-08-16 05:52:56 +0800

    add some script

---
+++ csm/script/libraw.py
@@ -0,0 +1,36 @@
+
+from ._common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    list_name = addDependency("lcms2" , list_name,getDependency)
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    # if(b_only_download):
+        # download_source(str_name,"https://github.com/madler/zlib.git")
+        # return
+        
+    STR_CFG = ''
+    STR_CFG += ' -DBUILD_TESTS=0'
+    if(dict_config['static']):
+        STR_CFG += " -DBUILD_SHARED_LIBS=0"
+    else:
+        STR_CFG += " -DBUILD_SHARED_LIBS=1"
+        
+    # LIBRAW_PATH
+    # STR_CFG += ' -DLIBRAW_PATH="' + os.getcwd() + '/../prebuild/LibRaw-master"'
+    STR_CFG += ' -DLIBRAW_PATH="' + os.getcwd() + '/../prebuild/LibRaw-0.20.0"'
+    
+    source_dir = os.getcwd() + '/../prebuild/LibRaw-cmake-master'
+    
+    configure(str_name,dict_config,STR_CFG,"",source_dir)
+    build(str_name,dict_config)
+    install(str_name,dict_config)
+    
+    
---
+++ csm/script/opencv.py
@@ -0,0 +1,48 @@
+
+from ._common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    # list_name = addDependency("zlib" , list_name,getDependency)
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    # if(b_only_download):
+        # download_source(str_name,"https://github.com/madler/zlib.git")
+        # return
+    
+    install_dir = dict_config['install_dir'] + '/' + my_build_and_install_dir(dict_config)
+    install_dir = install_dir.replace('\\','/')	
+    
+    STR_CFG = ''
+    # STR_CFG += ' -DOPENCV_FORCE_3RDPARTY_BUILD=1'
+    STR_CFG += ' -DBUILD_TESTS=0'
+    STR_CFG += ' -DBUILD_opencv_python3=0'
+    STR_CFG += ' -DWITH_IPP=0'
+    STR_CFG += ' -DWITH_FFMPEG=0'
+    
+    if(dict_config['static']):
+        STR_CFG += " -DBUILD_SHARED_LIBS=0"
+    else:
+        STR_CFG += " -DBUILD_SHARED_LIBS=1"
+        
+    # STR_CFG += ' -Dminizip-ng_INCLUDE_DIR=' + install_dir + '/include/minizip/'
+    
+    # STR_CFG += ' -Dyaml-cpp_LIBRARY=' + install_dir + '/lib/yaml-cpp.lib'
+    
+    # STR_CFG += ' -Dlcms2_INCLUDE_DIR=' + install_dir + '/include/'
+    # STR_CFG += ' -Dlcms2_LIBRARY=' + install_dir + '/lib/lcms2.lib'
+        
+    source_dir = os.getcwd() + '/../prebuild/opencv-4.x'
+    # source_dir = os.getcwd() + '/../prebuild/opencv-2.4.13'
+    
+    configure(str_name,dict_config,STR_CFG,"",source_dir)
+    build(str_name,dict_config)
+    install(str_name,dict_config)
+    
+    
