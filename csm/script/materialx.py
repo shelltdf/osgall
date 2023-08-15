7aebfbb86f65470dfb4fb6e7d81b45086772a839
shell_tdf <shell_tdf@126.com>
2023-08-16 05:07:10 +0800

    add some script

---
+++ csm/script/materialx.py
@@ -0,0 +1,29 @@
+
+from ._common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    # if(b_only_download):
+        # download_source(str_name,"https://github.com/madler/zlib.git")
+        # return
+        
+    STR_CFG = ''
+    if(dict_config['static']):
+        STR_CFG += " -DMATERIALX_BUILD_SHARED_LIBS=0"
+    else:
+        STR_CFG += " -DMATERIALX_BUILD_SHARED_LIBS=1"
+        
+    source_dir = os.getcwd() + '/../prebuild/MaterialX-main'
+    
+    configure(str_name,dict_config,STR_CFG,"",source_dir)
+    build(str_name,dict_config)
+    install(str_name,dict_config)
+    
+    
