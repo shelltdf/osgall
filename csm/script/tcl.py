2447a4c35042ab23d4b75a348a38b2c6a9dcbe9c
shell_tdf <shell_tdf@126.com>
2023-08-22 21:43:57 +0800

    add occt script

---
+++ csm/script/tcl.py
@@ -0,0 +1,48 @@
+
+from ._common import *
+
+def getDependency( str_name ,getDependency):
+    list_name = []
+    
+    return list_name + [str_name]
+    
+    
+def SBI( str_name , b_only_download ,dict_config, getLibrary ):
+    # print(str_name)
+    
+    # if(b_only_download):
+        # download_source(str_name,"https://github.com/madler/zlib.git")
+        # return
+    
+    install_dir = dict_config['install_dir'] + '/' + my_build_and_install_dir(dict_config)
+    install_dir = install_dir.replace('\\','/')	
+    
+    # STR_CFG = ''
+    # STR_CFG += ' -DBUILD_TESTING=0'
+    # if(dict_config['static']):
+        # STR_CFG += " -DBUILD_SHARED_LIBS=0"
+    # else:
+        # STR_CFG += " -DBUILD_SHARED_LIBS=1"
+        
+    source_dir = os.getcwd() + '/../prebuild/tcl8.6.13'
+    
+    # configure(str_name,dict_config,STR_CFG,"",source_dir)
+    # build(str_name,dict_config)
+    # install(str_name,dict_config)
+    
+    os.chdir( dict_config['install_dir'] )
+    if not os.path.isdir(my_build_and_install_dir(dict_config)):
+        os.mkdir( my_build_and_install_dir(dict_config) );
+    
+    os.chdir( source_dir + "/win")
+    
+    system_ret = os.system('nmake -f makefile.vc release')
+    system_ret = os.system('nmake -f makefile.vc install INSTALLDIR=' + install_dir)
+    
+    os.chdir( CWD )
+
+    if(system_ret != 0):
+        # print("error = " + CWD)
+        print("error = " + source_dir)
+        sys.exit(1)
+        
